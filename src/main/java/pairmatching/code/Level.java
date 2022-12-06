23cef63eb248029db0afe009dac8cbce87e49554
wnsgh12s <junho1273@gmail.com>
2022-12-06 22:09:12 +0900

    Feat: 전체레벨에 포함된 미션을 입력받는 기능

--- src/main/java/pairmatching/code/Level.java
+++ src/main/java/pairmatching/code/Level.java
@@ -1,27 +1,55 @@
 package pairmatching.code;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public enum Level {
-    LEVEL1("레벨1"),
-    LEVEL2("레벨2"),
+    LEVEL1("레벨1", List.of(Mission.RACING, Mission.LOTTO, Mission.BASEBALL)),
+    LEVEL2("레벨2", List.of(Mission.SHOPPING_BASKET, Mission.PAYMENT, Mission.SUBWAY_MAP)),
     LEVEL3("레벨3"),
-    LEVEL4("레벨4"),
+    LEVEL4("레벨4", List.of(Mission.PERFORMANCEIMPROVEMENT, Mission.DISTRIBUTE)),
     LEVEL5("레벨5");
 
     private final String name;
+    private final List<Mission> missions;
+
     Level(final String name) {
-        this.name =name;
+        this.name = name;
+        missions = new ArrayList<>();
     }
 
-    public static Level of(String input) {
-        return Arrays.stream(values()).filter(level -> level.name.equals(input))
+
+    Level(String name, List<Mission> missions) {
+        this.name = name;
+        this.missions = missions;
+    }
+
+    public static Level of(String input, Mission mission) {
+        return Arrays.stream(values())
+                .filter(level -> level.name.equals(input))
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(""));
+                .orElseThrow(() -> new IllegalArgumentException(""))
+                .findMission(mission);
+    }
+
+    private Level findMission(Mission mission) {
+        if( missions.contains(mission)){
+            return this;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private static Level getLevel(Mission mission, Level findedLevel) {
+        if(findedLevel.missions.contains(mission)){
+            return findedLevel;
+        }
+        throw new IllegalArgumentException();
     }
 
     public String getName() {
         return name;
     }
-    //docs? 테스트 ?
 }
